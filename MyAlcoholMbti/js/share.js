51bfd28db2ed178f645fefdac0c3ca65ef1b9937
jxhxx <72649564+jxhxx@users.noreply.github.com>
2022-06-03 15:37:24 +0900

    배포 완료 / 공유 기능 진행중..

---
+++ MyAlcoholMbti/js/share.js
@@ -0,0 +1,59 @@
+const url='https://alcoholmbti.netlify.app/';
+
+function setShare(){
+    var resultImg = document.querySelector('#resultImg');
+    var resultAlt = resultImg.firstElementChild.alt;
+
+    const shareTitle = '술bti';
+    const shareDes = infoList[resultAlt].name;
+    const shareImage = url + 'canvas/share-image-' + resultAlt + '.png';
+    const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+
+        buttons: [
+        {
+            title: '결과확인하기',
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+        ]
+    });
+}
+
+function kakaoShare(){
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: shareTitle,
+            description: shareDes,
+            imageUrl: shareImage,
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+
+        buttons: [
+        {
+            title: '결과확인하기',
+            link: {
+                mobileWebUrl: shareURL,
+                webUrl: shareURL
+            },
+        },
+        ]
+    });
+}
